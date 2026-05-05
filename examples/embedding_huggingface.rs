7f62ef7907bc3a080b86268241ce3914a43bff86
Mark Masterson <markmasterson@gmail.com>
2026-05-04 23:35:10 -0500

    Use chunk-safe embedding helpers in Hugging Face example

--- examples/embedding_huggingface.rs
+++ examples/embedding_huggingface.rs
@@ -3,9 +3,7 @@
 //! Run with:
 //! `cargo run --example embedding_huggingface --features embedding-huggingface`
 
-#![cfg(feature = "embedding-huggingface")]
-
-use aletheiadb::embeddings::{EmbedderBuilder, EmbeddingResult};
+use aletheiadb::embeddings::{EmbedderBuilder, embed_data_to_dense_iter, embed_query};
 use aletheiadb::{AletheiaDB, PropertyMapBuilder};
 
 #[tokio::main]
@@ -20,34 +18,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "Python is common in data science",
         "JavaScript powers browser applications",
     ];
-    let embeddings = dense_embeddings(embedder.embed(&documents, Some(32), None).await?)?;
+    let data = embed_query(&documents, &embedder, None).await?;
 
     let db = AletheiaDB::new()?;
-    for (doc, embedding) in documents.iter().zip(embeddings.iter()) {
-        db.create_node(
-            "TechDoc",
-            PropertyMapBuilder::new()
-                .insert("content", *doc)
-                .insert_vector("embedding", embedding)
-                .build(),
-        )?;
+    for item in embed_data_to_dense_iter(data, None) {
+        let item = item?;
+        if let Some(content) = item.text {
+            db.create_node(
+                "TechDoc",
+                PropertyMapBuilder::new()
+                    .insert("content", content)
+                    .insert_vector("embedding", &item.embedding)
+                    .build(),
+            )?;
+        }
     }
 
     Ok(())
-}
-
-fn dense_embeddings(
-    results: Vec<EmbeddingResult>,
-) -> Result<Vec<Vec<f32>>, Box<dyn std::error::Error>> {
-    results
-        .iter()
-        .map(EmbeddingResult::to_dense)
-        .collect::<Result<Vec<_>, _>>()
-        .map_err(Into::into)
-}
-
-#[cfg(not(feature = "embedding-huggingface"))]
-fn main() {
-    eprintln!("This example requires the 'embedding-huggingface' feature.");
-    std::process::exit(1);
 }
