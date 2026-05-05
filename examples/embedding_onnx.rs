6f3d9b85edb4234d6216e1e1a81b9f053595a2f6
Mark Masterson <markmasterson@gmail.com>
2026-05-04 23:35:37 -0500

    Use chunk-safe embedding helpers in ONNX example

--- examples/embedding_onnx.rs
+++ examples/embedding_onnx.rs
@@ -6,9 +6,7 @@
 //! Run with:
 //! `cargo run --example embedding_onnx --features embedding-onnx`
 
-#![cfg(feature = "embedding-onnx")]
-
-use aletheiadb::embeddings::{Dtype, Embedder, EmbeddingResult};
+use aletheiadb::embeddings::{Dtype, Embedder, embed_data_to_dense_iter, embed_query};
 use aletheiadb::{AletheiaDB, PropertyMapBuilder};
 
 #[tokio::main]
@@ -31,34 +29,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "Local models provide privacy and low latency",
         "Embedding models can run on CPU or GPU",
     ];
-    let embeddings = dense_embeddings(embedder.embed(&documents, Some(32), None).await?)?;
+    let data = embed_query(&documents, &embedder, None).await?;
 
     let db = AletheiaDB::new()?;
-    for (doc, embedding) in documents.iter().zip(embeddings.iter()) {
-        db.create_node(
-            "Document",
-            PropertyMapBuilder::new()
-                .insert("text", *doc)
-                .insert_vector("embedding", embedding)
-                .build(),
-        )?;
+    for item in embed_data_to_dense_iter(data, None) {
+        let item = item?;
+        if let Some(text) = item.text {
+            db.create_node(
+                "Document",
+                PropertyMapBuilder::new()
+                    .insert("text", text)
+                    .insert_vector("embedding", &item.embedding)
+                    .build(),
+            )?;
+        }
     }
 
     Ok(())
-}
-
-fn dense_embeddings(
-    results: Vec<EmbeddingResult>,
-) -> Result<Vec<Vec<f32>>, Box<dyn std::error::Error>> {
-    results
-        .iter()
-        .map(EmbeddingResult::to_dense)
-        .collect::<Result<Vec<_>, _>>()
-        .map_err(Into::into)
-}
-
-#[cfg(not(feature = "embedding-onnx"))]
-fn main() {
-    eprintln!("This example requires the 'embedding-onnx' feature.");
-    std::process::exit(1);
 }
